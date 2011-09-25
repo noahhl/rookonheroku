ee117dcc8d722fb2fbe664060bf28326fc360880
Noah Lorang <noah@datarobots.com>
2011-09-25 17:04:47 -0400

    Simpler test

--- demo.R
+++ demo.R
@@ -1,15 +1,5 @@
 require('Rook')
-TestApp <- Rook::Builder$new(
-  Rook::App$new(function(env) {
-    req <- Rook::Request$new(env)
-    res <- Rook::Response$new()
-    res$write("Ok, I heard you loud and clear") 
-    res$finish()    
-  })
-)
-
 rook <- Rhttpd$new()
-rook$add(name="TestApp", app=TestApp)
 rook$start(listen="0.0.0.0", port=as.numeric(Sys.getenv("PORT")))
 
 while(T) {
